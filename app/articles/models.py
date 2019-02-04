7fcddf89255dd1b505ac12dc00dbb4337e89f3a2
Vishal Nagda <vishalnagda1@gmail.com>
2019-02-04 17:01:51 +0530

    create article model

--- app/articles/models.py
+++ app/articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, default='')
+    text = models.TextField(default='')
+    archive = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
