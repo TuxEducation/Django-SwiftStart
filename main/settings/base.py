7611696da244de426a1b485678becbec6b02d8eb
Vishal Nagda <vishalnagda1@gmail.com>
2019-07-08 16:59:41 +0530

    add comments in the base settings file

--- main/settings/base.py
+++ main/settings/base.py
@@ -24,13 +24,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
     'rest_framework',
+
+    # Local Apps (Your project's apps)
     'app.articles',
 ]
 
